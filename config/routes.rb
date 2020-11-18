6ffefc7e2d7bd9e69c7d34a321bcdb3b52e9d489
Puneet Gupta <puneet@gmail.com>
2020-11-18 14:51:01 +0400

    added bootstrap and changed designs

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :commercial_units
   resources :complex_buildings
   resources :houses
+
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
