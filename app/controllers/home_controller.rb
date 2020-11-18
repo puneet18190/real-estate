a391b5498f41f233f2aa2a1fc92f3846b0a218d4
Puneet Gupta <puneet@gmail.com>
2020-11-18 17:36:32 +0400

    added carousel slider

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+  	@houses = House.all
+    @commercial_units = CommercialUnit.all
+    @complex_buildings = ComplexBuilding.all
   end
 end
