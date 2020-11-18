d78e614579e213a700c0aed740c558c0081cc6f4
Puneet Gupta <puneet@gmail.com>
2020-11-18 20:05:33 +0400

    changed design of list on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
-  	@houses = House.all
-    @commercial_units = CommercialUnit.all
-    @complex_buildings = ComplexBuilding.all
+    @data = []
+    @data << House.all.order('created_at desc')
+    @data << CommercialUnit.all.order('created_at desc')
+    @data << ComplexBuilding.all.order('created_at desc')
+    @data = @data.compact.flatten
   end
 end
